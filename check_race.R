c5e0fb7d3cfb706e6d16cada0f2d5300bc27155a
Joshua Horsley <24281654+joshhorsley@users.noreply.github.com>
2022-12-10 19:13:35 +1100

    Added race check script

---
+++ check_race.R
@@ -0,0 +1,10 @@
+
+# Check race --------------------------------------------------------------
+
+
+
+date_check <- "2022-12-10"
+
+p_races <- dt_season[date_ymd==date_check,list(list((plotly_race(dt_all_long, tri_cols, season, race_number, course,is_champ)))), by = .(course)]
+
+p_races$V1
